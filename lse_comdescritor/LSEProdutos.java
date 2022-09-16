a8cb21d1a71365c67c028266d2b04aa3182727aa
Amanda Laís <laisamanda2015@gmail.com>
2022-09-16 11:34:46 +0000

    list in progress

--- lse_comdescritor/LSEProdutos.java
+++ lse_comdescritor/LSEProdutos.java
@@ -7,5 +7,4 @@ public class LSEProdutos {
     private int qtd;
 
     
-    
 }
--- lse_comdescritor/Produto.java
+++ lse_comdescritor/Produto.java
@@ -10,12 +10,45 @@ public class Produto implements Comparable <Produto>{
         this.codigo = cod;
     }
 
+    public void setCodigo (String codigo){
+        this.codigo = codigo;
+    }
+
+    public void setDescricao (String desricao){
+        this.descricao = descricao;
+    }
+
+    public void setEstoque (int estoque){
+        this.estoque = estoque;
+    }
+
+    public void setPreco (double preco){
+        this.preco = preco;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public int getEstoque(){
+        return estoque;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
     @Override
     public int compareTo(Produto p){
         return this.codigo.compareTo(p.codigo);
     }
 
-    // construtor
-    //get e set
-    // compare to
+    @Override
+    public String toString(){
+        return this.codigo + " " + this.descricao + " " + this.preco + " " + this.estoque;
+    }
 }
