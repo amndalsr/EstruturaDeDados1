406c5a736dac135eedce07baa93cc7135c0c2451
Amanda Laís <laisamanda2015@gmail.com>
2022-09-27 11:43:07 +0000

    list in progress

--- atividade05_lse_ordenada/LSEOrdenada.java
+++ atividade05_lse_ordenada/LSEOrdenada.java
@@ -5,4 +5,12 @@ public class LSEOrdenada  <T extends Comparable <T>>{
     private LSENode<T> primeiro;
     private LSENode<T> ultimo;
     private int qtd;
+
+    public boolean isEmpty() {
+        if (this.primeiro == null && this.ultimo == null && this.qtd == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
