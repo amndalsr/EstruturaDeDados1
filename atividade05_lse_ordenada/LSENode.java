8f8f3ebacbad2659d8634b5385a2558033811d58
Amanda Laís <laisamanda2015@gmail.com>
2022-09-27 11:37:27 +0000

    list in progress

--- atividade05_lse_ordenada/LSENode.java
+++ atividade05_lse_ordenada/LSENode.java
@@ -1,5 +1,26 @@
 package atividade05_lse_ordenada;
 
-public class LSENode {
-    
+public class LSENode<T extends Comparable<T>> {
+    private T info;
+    private LSENode<T> prox;
+
+    public LSENode(T valor) {
+        this.info = valor;
+    }
+
+    public void setInfo(T valor) {
+        this.info = valor;
+    }
+
+    public T getInfo() {
+        return this.info;
+    }
+
+    public void setProx(LSENode<T> novoProx) {
+        this.prox = novoProx;
+    }
+
+    public LSENode<T> getProx() {
+        return this.prox;
+    }
 }
