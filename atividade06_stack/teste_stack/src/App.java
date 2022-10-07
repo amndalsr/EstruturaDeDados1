db91c355fbd51d2a79e1bdcbd02845748d3e8148
amndalsr <laisamanda2015@gmail.com>
2022-10-07 16:50:32 -0300

    lista concluida ✔️

--- atividade06_stack/teste_stack/src/App.java
+++ atividade06_stack/teste_stack/src/App.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
 import br.unicap.edi.stack.Stack;
 
+
 public class App {
     public static void main(String[] args) throws Exception {
-    
+        Scanner in = new Scanner (System.in);
+        Stack<Integer> pilha = new Stack();
+        int num, resto, quociente;
+
+        System.out.println("Digite um número inteiro em decimal: ");
+        num = in.nextInt();
+
+        do {
+            quociente = num / 2;
+            resto = num % 2;
+            pilha.push(resto);
+            num = quociente;
+        } while (num != 0);
+
+        while(pilha.isEmpty() == false) {
+            resto = pilha.pop();
+            System.out.println(resto);
+        }
     }
 }
