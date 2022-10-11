0abfe901ce6fe2f554f0299d6fdc2f6c7ff1ff0e
amndalsr <laisamanda2015@gmail.com>
2022-10-10 23:30:43 -0300

    initial commit

---
+++ atividade07_queue/Queue/src/br/unicap/edi/queue/Queue.java
@@ -0,0 +1,21 @@
+package br.unicap.edi.queue;
+
+public class Queue<T> {
+    private int [] dados;
+    private int inicio, fim;
+
+    public void enQueue(T valor){
+    }
+
+    public int deQueue(){
+    }
+
+    public int head(){
+    }
+
+    public boolean isEmpty(){
+    }
+
+    public boolean isFull(){
+    }
+}
