729e3f44c3ac42991fb25bb29512fd57c191d67a
Amanda Laís <laisamanda2015@gmail.com>
2022-11-04 13:58:21 +0000

    in progress

--- listacircular/listacircular.java
+++ listacircular/listacircular.java
@@ -71,8 +71,55 @@ public class LDECircular <T extends Comparable<T>> {
         }
     }
 
+    public void removerInicio () {
+        if (this.isEmpty() == true) {
+            System.out.println("vazia");
+        } else if (this.qtd == 1) {
+            this.primeiro =  null;
+            this.ultimo = null;
+            this.qtd--;
+        } else {
+            this.primeiro = this.primeiro.getProx();
+            this.qtd--;
+            this.primeiro.setAnterior(this.ultimo);
+            this.ultimo.setProximo(this.primeiro);
+        }
+    }   
     
+    public void inserirNoFinal (T valor) {
+        LDENode<T> novo = new LDENode(valor);
+        if (this.isEmpty() == true) {
+            this.primeiro = novo;
+            this.ultimo = novo;
+            this.qtd = 0;
+            this.primeiro.setAnterior(this.ultimo);
+            this.ultimo.setProximo(this.primeiro);
+        } else {
+            novo.setAnterior(this.ultimo);
+            this.ultimo.setProximo(novo);
+            this.ultimo = novo;
+            this.qtd++;
+            this.primeiro.setAnterior(this.ultimo);
+            this.ultimo.setProximo(this.primeiro);
+        }
+    }
+    
+    public void removerFinal () {
+        if (this.isEmpty() == true) {
+            System.out.println("vazia");
+        } else if (this.qtd == 1) {
+            this.primeiro =  null;
+            this.ultimo = null;
+            this.qtd--;
+        } else {
+            this.ultimo = this.ultimo.getAnt();
+            this.qtd--;
+            this.primeiro.setAnterior(this.ultimo);
+            this.ultimo.setProximo(this.primeiro);
+        }
+    }
 
+    public void exibirTodos() {
+
+    }
 }
-
-//criar metodo exibir
