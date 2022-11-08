722cffc3cef883565db231a8b0edc2620e809cf8
Amanda Laís <laisamanda2015@gmail.com>
2022-11-08 13:37:49 +0000

    lista concluida ✔️

--- lde_circular/ldecircular/src/br/unicap/edi/lde/LDECircular.java
+++ lde_circular/ldecircular/src/br/unicap/edi/lde/LDECircular.java
@@ -131,27 +131,78 @@ public class LDECircular <T extends Comparable<T>>{
         if (retorno == null) {
             System.out.println("Valor não encontrado.");
         } else if (this.qtd == 1) {
-            this.primeiro = null;
-            this.ultimo = null;
-            this.qtd = 0;
-        } else if (retorno == this.primeiro) {
-            this.primeiro = this.primeiro.getProx();
-            this.qtd--;
-            this.primeiro.setAnterior(this.ultimo);
-            this.ultimo.setProximo(this.primeiro);
-        } else if (retorno == this.ultimo) {
-            this.ultimo = this.ultimo.getAnt();
-            this.qtd--;
-            this.primeiro.setAnterior(this.ultimo);
-            this.ultimo.setProximo(this.primeiro);
+            if (valor.compareTo(this.primeiro.getInfo()) == 0) {
+                this.primeiro = null;
+                this.ultimo = null;
+                this.qtd = 0;
+            } else {
+                System.out.println("Valor não encontrado!");
+            }
         } else {
-            anterior = retorno.getAnt();
-            proximo = retorno.getProx();
-            anterior.setProximo(proximo);
-            proximo.setAnterior(anterior);
-            this.qtd--;
+            retorno = this.buscar(valor);
+            if (retorno != null) {
+                anterior = retorno.getAnt();
+                proximo = retorno.getProx();
+                anterior.setProximo(proximo);
+                proximo.setAnterior(anterior);
+                this.qtd--;
+                if (retorno == this.primeiro) {
+                    this.primeiro = proximo;
+                } else if (retorno == this.ultimo) {
+                    this.ultimo = anterior;
+                }
+            } else {
+                System.out.println("Valor não encontrado!");
+            }
+        }
+    }
+
+    public void inserirOrdemDecrescente (T valor) {
+        LDENode<T> novo = new LDENode(valor);
+        LDENode<T> aux, anterior;
+
+        if (this.isEmpty()) {
+            this.primeiro = novo;
+            this.ultimo = novo;
+            this.qtd++;
             this.primeiro.setAnterior(this.ultimo);
             this.ultimo.setProximo(this.primeiro);
+        } else if (valor.compareTo(this.primeiro.getInfo()) > 0) {
+            novo.setProximo(this.primeiro);
+            this.primeiro.setAnterior(novo);
+            this.primeiro = novo;
+            this.qtd++;
+            this.primeiro.setAnterior(this.ultimo);
+            this.ultimo.setProximo(this.primeiro);
+        } else if (valor.compareTo(this.primeiro.getInfo()) == 0) {
+            System.out.println("Valor repetido, inserção não realizada!");
+        } else if (valor.compareTo(this.ultimo.getInfo()) < 0) {
+            this.ultimo.setProximo(novo);
+            novo.setAnterior(this.ultimo);
+            this.ultimo = novo;
+            this.qtd++;
+            this.primeiro.setAnterior(this.ultimo);
+            this.ultimo.setProximo(this.primeiro);
+        } else if (valor.compareTo(this.ultimo.getInfo()) == 0) {
+            System.out.println("Valor repetido, inserção não realizada!");
+        } else {
+            aux = this.primeiro.getProx();
+            while (true) {
+                if (valor.compareTo(aux.getInfo()) == 0) {
+                    System.out.println("Valor repetido, inserção não realizada!");
+                    break;
+                } else if (valor.compareTo(aux.getInfo()) > 0) {
+                    anterior = aux.getAnt();
+                    anterior.setProximo(novo);
+                    aux.setAnterior(novo);
+                    novo.setProximo(aux);
+                    novo.setAnterior(anterior);
+                    this.qtd++;
+                    break;
+                } else {
+                    aux = aux.getProx();
+                }
+            }
         }
     }
 }
