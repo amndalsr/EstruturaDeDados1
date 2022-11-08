5b944f07461a1d657efe65e52c8ef3f07c4cf49f
amndalsr <laisamanda2015@gmail.com>
2022-11-07 22:15:26 -0300

    lista concluida ✔️

---
+++ atividade09_questao02/lde/src/br/unicap/edi/led/LDENode.java
@@ -0,0 +1,35 @@
+package br.unicap.edi.led;
+
+public class LDENode <T extends Comparable<T>> {
+    private LDENode<T> anterior;
+    private T info;
+    private LDENode<T> proximo;
+
+    public LDENode (T valor) {
+        this.info = valor;
+    }
+
+    public void setAnterior(LDENode<T> novoAnterior) {
+        this.anterior = novoAnterior;
+    }
+
+    public LDENode<T> getAnt() {
+        return this.anterior;
+    }
+
+    public void setInfo (T valor) {
+        this.info = valor;
+    }
+
+    public T getInfo() {
+        return this.info;
+    }
+
+    public void setProximo (LDENode<T> novoProximo) {
+        this.proximo = novoProximo;
+    }
+
+    public LDENode<T> getProx() {
+        return this.proximo;
+    }
+}
---
+++ atividade09_questao02/testa_lde/src/App.java
@@ -0,0 +1,26 @@
+import br.unicap.edi.led.LDE;
+import br.unicap.edi.led.LDENode;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        LDE<Integer> lista1 = new LDE();
+
+        lista1.inserir(5);
+        lista1.inserir(2);
+        lista1.inserir(13);
+        lista1.inserir(7);
+
+        System.out.println("Valores na lista: ");
+        lista1.exibirTodos();
+
+        System.out.println("Valores na lista em ordem crescente: ");
+        lista1.exibirAoContrario();
+
+        lista1.verificar(9);
+
+        lista1.remover(2);
+        lista1.exibirTodos();
+
+
+    }
+}
